cb8bd05b8e6ed64bd4ed83d1d4ea23d1322736c5
yamatatsu <the.kinnikuman@gmail.com>
2025-06-02 18:11:33 +0900

    cognitoと接続

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,78 @@
 import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import * as logs from "aws-cdk-lib/aws-logs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as assets from "aws-cdk-lib/aws-ecr-assets";
+import { bedrock } from '@cdklabs/generative-ai-cdk-constructs';
+import * as path from "path";
 
 const app = new cdk.App();
 const stack = new cdk.Stack(app,'PlayOpenWebuiCognitoBedrock');
+
+
+// User pool
+const userPool = new cognito.UserPool(stack, "Default", {
+	selfSignUpEnabled: false,
+	signInAliases: {
+		username: false,
+		email: true,
+	},
+	passwordPolicy: {
+		tempPasswordValidity: cdk.Duration.days(7),
+		requireLowercase: false,
+		requireUppercase: false,
+		requireDigits: false,
+		requireSymbols: false,
+		minLength: 8,
+	},
+	email: cognito.UserPoolEmail.withCognito(),
+	accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+	deviceTracking: {
+		challengeRequiredOnNewDevice: true,
+		deviceOnlyRememberedOnUserPrompt: true,
+	},
+
+	removalPolicy: cdk.RemovalPolicy.DESTROY,
+	deletionProtection: false,
+});
+userPool.addDomain("CustomDomain", {
+	cognitoDomain: {
+		domainPrefix: 'yamatatsu-open-webui-test',
+	}
+});
+userPool.addClient("UserPoolClient", {
+	generateSecret: true,
+	oAuth: {
+		callbackUrls: ['http://localhost:8080/oauth/oidc/callback'],
+		logoutUrls: ['http://localhost:8080'],
+		flows: { authorizationCodeGrant: true },
+		scopes: [
+			cdk.aws_cognito.OAuthScope.OPENID,
+			cdk.aws_cognito.OAuthScope.EMAIL,
+			cdk.aws_cognito.OAuthScope.PROFILE,
+		],
+	},
+	authFlows: {
+		userSrp: true,
+		user: true,
+		userPassword: true,
+	},
+	preventUserExistenceErrors: true,
+	supportedIdentityProviders: [cdk.aws_cognito.UserPoolClientIdentityProvider.COGNITO],
+});
+
+const openWebUIStorage = new s3.Bucket(stack, "OpenWebUIStorage", {
+	bucketName: `open-webui-storage-${stack.account}-${stack.region}`,
+	blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+	encryption: s3.BucketEncryption.S3_MANAGED,
+	enforceSSL: true,
+	// 削除されては困るならコメントアウトして再デプロイ
+	removalPolicy: cdk.RemovalPolicy.DESTROY,
+	autoDeleteObjects: true,
+});
 
 const vpc = new ec2.Vpc(stack, "Vpc", {
 	natGateways: 0
